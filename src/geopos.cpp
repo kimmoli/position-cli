116efc0a3f012704ec380e65cb495cdcac4ae547
kimmoli <kimmo.lindholm@eke.fi>
2015-01-18 22:21:55 +0200

    add more prints for verbose

--- src/geopos.cpp
+++ src/geopos.cpp
@@ -35,14 +35,19 @@ void Geopos::positionUpdated(const QGeoPositionInfo &info)
     if (m_verbose)
     {
         printf("timestamp %s\n", qPrintable(info.timestamp().toString()));
-        printf("latitude %f\n", info.coordinate().latitude());
-        printf("longitude %f\n", info.coordinate().longitude());
+        printf("  valid %s\n", info.isValid() ? "yes":"no");
+        printf("  latitude %f\n", info.coordinate().latitude());
+        printf("  longitude %f\n", info.coordinate().longitude());
+        printf("  altitude %f\n", info.coordinate().altitude());
+        printf("  ground speed %f\n", info.attribute(QGeoPositionInfo::GroundSpeed));
+        printf("  horizontal accuracy %f\n", info.attribute(QGeoPositionInfo::HorizontalAccuracy));
     }
     else
     {
-        printf("%s %f %f\n", qPrintable(info.timestamp().toString()),
+        printf("%s %f %f%s\n", qPrintable(info.timestamp().toString()),
                                 info.coordinate().latitude(),
-                                info.coordinate().longitude());
+                                info.coordinate().longitude(),
+                                info.isValid() ? "":" invalid");
     }
 
     if (!m_repeat)
